5af712708909e6929761393acad6d1a98eb9bf8e
unknown <akshayaworkuse369@gmail.com>
2026-04-10 16:40:48 +0530

    done for,while loop

--- sample.js
+++ sample.js
@@ -54,3 +54,27 @@ switch (nuber) {
     default:
         console.log("Nuber is greater than 2.");
 };
+for (let i =0; i<=3; i++) {
+    console.log("HELLO");
+}
+for (let i=0; i<10; i++) {
+    if (i%2==0) {
+        console.log(i);
+    }
+} 
+for (let i=0; i<10; i++ )  {
+    if (i % 2 != 0)
+        console.log(i);
+}
+let i =0;
+while(i<=10) {
+    console.log(i); 
+    i++;
+}
+let j=0;
+while(j<=10) {
+    if (j % 2 == 0){
+        console.log("even numbers",j);
+    }
+    j++;
+}
