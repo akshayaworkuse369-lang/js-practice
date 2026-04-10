a910c2d4cc42dc9955aa8c73712690d06d8ebe5f
unknown <akshayaworkuse369@gmail.com>
2026-04-10 14:34:15 +0530

    initial commit

--- sample.js
+++ sample.js
@@ -6,3 +6,23 @@ let s="hello , world!";
 console.log(s)
 let bool = true
 console.log(bool)
+let empty = null;
+console.log(empty)
+let undefined;
+console.log(undefined);
+const sum = 5 + 3; // Addition
+const diff = 10 - 2; // Subtraction
+const p = 4 * 2; // Multiplication
+const q = 8 / 2; // Division
+console.log(sum, diff, p, q);
+let n=10
+n +=5;
+n -=3;
+n *=2;
+n /=4;
+console.log(n)
+const c = true, b = false;
+console.log(c && b); // Logical AND
+console.log(c || b); // Logical OR
+console.log(10 > 5);
+
